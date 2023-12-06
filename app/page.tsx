6eda493a6f5eeed4404405f1d3ff5a4de29872ef
Zack Hanni <zack.hannI@gmail.com>
2023-12-06 17:06:07 -0500

    show landing page before starting the feeling questions

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,26 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect, useState } from "react";
 import FeelingSurveyApp from "./components/FeelingSurveyApp";
+import PasswordPrompt from "./components/PasswordPrompt";
 
 export default function Home() {
-  return (
-    <main className="">
-      <FeelingSurveyApp />
-    </main>
-  );
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const userPassword = window.sessionStorage.getItem("password");
+    if (userPassword === process.env.NEXT_PUBLIC_PAGE_PASSWORD) {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  if (!isLoggedIn) {
+    return <PasswordPrompt />;
+  } else {
+    return (
+      <main className="flex flex-col items-center justify-center">
+        <FeelingSurveyApp />
+      </main>
+    );
+  }
 }
