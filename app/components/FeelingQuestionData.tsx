99cdcd54467b51f5a2e15bb21ad51999817e0c58
Zack Hanni <zack.hannI@gmail.com>
2023-12-06 17:04:16 -0500

    better organize feelingdata - still in progress

---
+++ app/components/FeelingQuestionData.tsx
@@ -0,0 +1,196 @@
+// array for questions and feelings
+const FeelingQuestionData = {
+  question: "How are you feeling?",
+  answers: [
+    {
+      label: "Happy",
+      questionTwo: {
+        question:
+          "You feel happy. It's wonderful to hear that. Can you be more specific?",
+        answers: [
+          {
+            label: "Playful",
+            questionThree: {
+              question: "This is question 3.",
+              answers: [{ label: "Aroused" }, { label: "Cheeky" }],
+            },
+          },
+          {
+            label: "Content",
+            questionThree: {
+              question: "This is question 3.",
+              answers: [{ label: "Free" }, { label: "Joyful" }],
+            },
+          },
+          {
+            label: "Interested",
+            questionThree: {
+              question: "This is question 3.",
+              answers: [{ label: "Aroused" }, { label: "Cheeky" }],
+            },
+          },
+          {
+            label: "Proud",
+            questionThree: {
+              question: "This is question 3.",
+              answers: [{ label: "Aroused" }, { label: "Cheeky" }],
+            },
+          },
+          {
+            label: "Accepted",
+            questionThree: {
+              question: "This is question 3.",
+              answers: [{ label: "Aroused" }, { label: "Cheeky" }],
+            },
+          },
+          {
+            label: "Powerful",
+            questionThree: {
+              question: "This is question 3.",
+              answers: [{ label: "Aroused" }, { label: "Cheeky" }],
+            },
+          },
+          {
+            label: "Peaceful",
+            questionThree: {
+              question: "This is question 3.",
+              answers: [{ label: "Aroused" }, { label: "Cheeky" }],
+            },
+          },
+          {
+            label: "Trusting",
+            questionThree: {
+              question: "This is question 3.",
+              answers: [{ label: "Aroused" }, { label: "Cheeky" }],
+            },
+          },
+          {
+            label: "Optimistic",
+            questionThree: {
+              question: "This is question 3.",
+              answers: [{ label: "Aroused" }, { label: "Cheeky" }],
+            },
+          },
+        ],
+      },
+    },
+    {
+      label: "Sad",
+      questionTwo: {
+        question:
+          "You feel sad. Remember that it is not permanent. Let's find a more specific description.",
+        answers: [
+          {
+            label: "Lonely",
+            questionThree: {
+              question: "This is question 3.",
+              answers: [{ label: "Aroused" }, { label: "Cheeky" }],
+            },
+          },
+          {
+            label: "Vulnerable",
+            questionThree: {
+              question: "This is question 3.",
+              answers: [{ label: "Aroused" }, { label: "Cheeky" }],
+            },
+          },
+          {
+            label: "Despair",
+            questionThree: {
+              question: "This is question 3.",
+              answers: [{ label: "Aroused" }, { label: "Cheeky" }],
+            },
+          },
+          {
+            label: "Guilty",
+            questionThree: {
+              question: "This is question 3.",
+              answers: [{ label: "Aroused" }, { label: "Cheeky" }],
+            },
+          },
+          {
+            label: "Depressed",
+            questionThree: {
+              question: "This is question 3.",
+              answers: [{ label: "Aroused" }, { label: "Cheeky" }],
+            },
+          },
+          {
+            label: "Hurt",
+            questionThree: {
+              question: "This is question 3.",
+              answers: [{ label: "Aroused" }, { label: "Cheeky" }],
+            },
+          },
+        ],
+      },
+    },
+    {
+      label: "Angry",
+      questionTwo: {
+        question:
+          "You feel angry. Take a deep breath. Let's find a more specific description.",
+        answers: [
+          {
+            label: "Let down",
+            questionThree: {
+              question: "This is question 3.",
+              answers: [{ label: "Aroused" }, { label: "Cheeky" }],
+            },
+          },
+          {
+            label: "Humiliated",
+            questionThree: {
+              question: "This is question 3.",
+              answers: [{ label: "Aroused" }, { label: "Cheeky" }],
+            },
+          },
+          {
+            label: "Bitter",
+            questionThree: {
+              question: "This is question 3.",
+              answers: [{ label: "Aroused" }, { label: "Cheeky" }],
+            },
+          },
+          {
+            label: "Mad",
+            questionThree: {
+              question: "This is question 3.",
+              answers: [{ label: "Aroused" }, { label: "Cheeky" }],
+            },
+          },
+          {
+            label: "Aggressive",
+            questionThree: {
+              question: "This is question 3.",
+              answers: [{ label: "Aroused" }, { label: "Cheeky" }],
+            },
+          },
+          {
+            label: "Frustrated",
+            questionThree: {
+              question: "This is question 3.",
+              answers: [{ label: "Aroused" }, { label: "Cheeky" }],
+            },
+          },
+          {
+            label: "Distant",
+            questionThree: {
+              question: "This is question 3.",
+              answers: [{ label: "Aroused" }, { label: "Cheeky" }],
+            },
+          },
+          {
+            label: "Critical",
+            questionThree: {
+              question: "This is question 3.",
+              answers: [{ label: "Aroused" }, { label: "Cheeky" }],
+            },
+          },
+        ],
+      },
+    },
+  ],
+};
+
+export default FeelingQuestionData;
