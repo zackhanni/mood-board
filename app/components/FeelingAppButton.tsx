6c0367c62cb131ecf20cb0ab0a62db208fb0bde5
Zack Hanni <zack.hannI@gmail.com>
2023-12-06 21:55:27 -0500

    rename button, create universal button

---
+++ app/components/FeelingAppButton.tsx
@@ -0,0 +1,15 @@
+interface ButtonProps {
+  onClick: () => void;
+  text: string;
+}
+
+export default function FeelingAppButton(props: ButtonProps) {
+  return (
+    <button
+      onClick={props.onClick}
+      className="px-4 py-2 flex w-full md:max-w-screen-sm font-bold rounded border-solid border-4 border-black hover:bg-yellow-100 items-center justify-center text-xl"
+    >
+      {props.text}
+    </button>
+  );
+}
