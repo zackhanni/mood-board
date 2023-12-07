a1845d8f9f47089ff06f9801a3b608029835826c
Zack Hanni <zack.hannI@gmail.com>
2023-12-06 21:55:46 -0500

    remove unused

--- app/components/FeelingQuestion.tsx
+++
@@ -1,9 +0,0 @@
-type FeelingQuestion = {
-  question: string;
-};
-
-const FeelingQuestion: React.FC<FeelingQuestion> = ({ question }) => {
-  return <h3>{question}</h3>;
-};
-
-export default FeelingQuestion;
