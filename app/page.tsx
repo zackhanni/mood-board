a1967f83607bf30c85b458e7bbd111d716ab0ddc
Zack Hanni <zack.hannI@gmail.com>
2023-12-10 21:25:36 -0500

    home - add title and change colors/css

--- app/page.tsx
+++ app/page.tsx
@@ -18,10 +18,15 @@ export default async function SignInPage() {
 
   return (
     <div className="w-full flex flex-col items-center justify-center min-h-screen py-2">
-      <div className="flex flex-col items-center mt-10 p-10 shadow-md">
-        <h1 className="mt-10 mb-4 text-4xl font-bold">Sign In</h1>
+      <div>
+        <h1 className="text-4xl font-bold text-center">Mood Board</h1>
+        <p>Learn about your feelings and track your daily emotions</p>
+      </div>
+
+      <div className="flex flex-col items-center mt-10 p-10 rounded-lg shadow-md hover:shadow-2xl">
+        <h2 className="mt-10 mb-4 text-4xl font-bold">Sign In</h2>
         <GoogleSignInButton />
-        <span className="text-2xl font-semibold text-white text-center mt-8">
+        <span className="text-2xl font-semibold text-black text-center mt-8">
           Or
         </span>
         {/* <CredentialsSignInButton /> */}
