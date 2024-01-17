f7ea4d078d55cb6cb0f52b9d711de3d20b48d440
Zack Hanni <zack.hannI@gmail.com>
2024-01-16 19:31:38 -0500

    revert prisma.ts back to previous state.
    still not sure what to do with this file yet

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,52 +1,53 @@
 // OG CODE
 
-// import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client'
 
-// let prisma: PrismaClient;
+let prisma: PrismaClient;
 
-// if (process.env.NODE_ENV === "production") {
-//   prisma = new PrismaClient();
-// } else {
-//   if (!(global as any).prisma) {
-//     (global as any).prisma = new PrismaClient();
-//   }
-//   prisma = (global as any).prisma;
-// }
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!(global as any).prisma) {
+    (global as any).prisma = new PrismaClient();
+  }
+  prisma = (global as any).prisma;
+}
 
-// export default prisma;
+export default prisma;
 
 // END OG CODE
 
 
-import jwt from 'jsonwebtoken';
+// import jwt from 'jsonwebtoken';
 
-import { PrismaClient } from '@prisma/client';
-export const prisma = new PrismaClient();
+// import { PrismaClient } from '@prisma/client';
+// export const prisma = new PrismaClient();
 
-export async function handleOAuthLogin(accessToken, refreshToken, profile) {
-  // Decode the ID token sent by Google
-  const decodedToken = jwt.decode(profile.id_token);
+// export async function handleOAuthLogin(accessToken, refreshToken, profile) {
+//   // Decode the ID token sent by Google
+//   const decodedToken = jwt.decode(profile.id_token);
  
- // Check if a user with the given email already exists
- const existingUser = await prisma.user.findUnique({
-  where: { email: decodedToken.email },
- });
+//  // Check if a user with the given email already exists
+//  const existingUser = await prisma.user.findUnique({
+//   where: { email: decodedToken.email },
+//   include: { entries: true },
+//  });
 
- if (existingUser) {
-  // If the user exists, log them in and fetch their data
-  // You'll need to implement the login and fetch data logic here
- } else {
-  // If the user doesn't exist, create a new user
-  const user = await prisma.user.create({
-    data: {
-      email: decodedToken.email,
-      name: decodedToken.name,
-      // Add any other fields you want to save from the token
-    },
-  });
- }
+//  if (existingUser) {
+//   // If the user exists, log them in and fetch their data
+//   // You'll need to implement the login and fetch data logic here
+//  } else {
+//   // If the user doesn't exist, create a new user
+//   const user = await prisma.user.create({
+//     data: {
+//       email: decodedToken.email,
+//       name: decodedToken.name,
+//       // Add any other fields you want to save from the token
+//     },
+//   });
+//  }
 
 
-}
+// }
 
 
