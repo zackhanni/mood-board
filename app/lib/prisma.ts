5d252d228516543a7db53e46a12424ae44856d3d
Zack Hanni <zack.hannI@gmail.com>
2024-01-23 10:20:15 -0500

    bring prisma.ts back to basics

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,53 +1,19 @@
-// OG CODE
-
 import { PrismaClient } from '@prisma/client'
 
-let prisma: PrismaClient;
+const prisma = new PrismaClient()
 
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!(global as any).prisma) {
-    (global as any).prisma = new PrismaClient();
-  }
-  prisma = (global as any).prisma;
-}
-
-export default prisma;
-
-// END OG CODE
-
-
-// import jwt from 'jsonwebtoken';
-
-// import { PrismaClient } from '@prisma/client';
-// export const prisma = new PrismaClient();
-
-// export async function handleOAuthLogin(accessToken, refreshToken, profile) {
-//   // Decode the ID token sent by Google
-//   const decodedToken = jwt.decode(profile.id_token);
- 
-//  // Check if a user with the given email already exists
-//  const existingUser = await prisma.user.findUnique({
-//   where: { email: decodedToken.email },
-//   include: { entries: true },
-//  });
-
-//  if (existingUser) {
-//   // If the user exists, log them in and fetch their data
-//   // You'll need to implement the login and fetch data logic here
-//  } else {
-//   // If the user doesn't exist, create a new user
-//   const user = await prisma.user.create({
-//     data: {
-//       email: decodedToken.email,
-//       name: decodedToken.name,
-//       // Add any other fields you want to save from the token
-//     },
-//   });
-//  }
-
-
+// async function main() {
+//   // ... you will write your Prisma Client queries here
 // }
 
-
+// main()
+//   .then(async () => {
+//     await prisma.$disconnect()
+//   })
+//   .catch(async (e) => {
+//     console.error(e)
+//     await prisma.$disconnect()
+//     process.exit(1)
+//   })
+  
+  export default prisma
