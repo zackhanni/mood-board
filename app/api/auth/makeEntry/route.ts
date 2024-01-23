b8d7f5dc703c5f1511d89dca98e0271fe10d3159
Zack Hanni <zack.hannI@gmail.com>
2024-01-23 16:29:52 -0500

    working - route to make a new Entry

--- app/api/auth/makeEntry/route.ts
+++ app/api/auth/makeEntry/route.ts
@@ -2,32 +2,28 @@ import { connectToDatabase } from "@/helpers/server-helpers"
 import prisma from "@/app/lib/prisma";
 import { NextResponse } from "next/server";
 
-export const POST = async (req: Request, session: any) => {
-    try {
-        // console.log(session);
-        const { feeling, thoughts, createdAt } = await req.json()
-        await connectToDatabase()
-        if (!session || !session.user || !session.user.id) {
-            throw new Error('User ID not found in session');
+export const POST = async (req: Request) => {
+  try {
+    const { feeling, thoughts, email } = await req.json()
+    if (!feeling || !thoughts || !email)
+      return NextResponse.json({ message: "Invalid Data " })
+    await connectToDatabase()
+    const entry = await prisma.entry.create({
+      data: {
+        feeling,
+        thoughts,
+        user: {
+          connect: {
+            email: email
+          }
         }
-        const newEntry = await prisma.entry.create({
-            data: {
-                feeling: feeling,
-                thoughts: thoughts,
-                createdAt: createdAt,
-                user: {
-                  connect: {
-                    // having trouble with Postman seeing the session ID
-                    id: session.user.id,
-                 },
-                },
-              },
-        })
-        return NextResponse.json({ newEntry }), { status: 201 }
-    } catch (error) {
-        console.log(error);
-        return NextResponse.json({ message: (error as Error).message }, { status: 500 });
-      } finally {
-        await prisma.$disconnect();
       }
+    })
+    return NextResponse.json({ entry }, { status: 201 })
+  } catch (error) {
+    console.log(error)
+    return NextResponse.json({ error: (error as Error).message, stack: (error as Error).stack },{ status: 500 })
+  } finally {
+    await prisma.$disconnect()
+  }
 }
