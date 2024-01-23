06f21a626cb4dd323c9a64d457b6b2545d531312
Zack Hanni <zack.hannI@gmail.com>
2024-01-23 10:21:23 -0500

    removed session provider from mental check in

--- app/mental-check-in/page.tsx
+++ app/mental-check-in/page.tsx
@@ -2,17 +2,15 @@
 
 import React from "react";
 import FeelingApp from "../components/FeelingApp";
-
-import { SessionProvider } from "next-auth/react";
 import Nav from "../components/Nav";
 
 export default function MentalCheckIn() {
   return (
-    <SessionProvider>
+    <>
       <Nav />
       <main className="flex flex-col items-center justify-center max-w-[90%]">
         <FeelingApp />
       </main>
-    </SessionProvider>
+    </>
   );
 }
