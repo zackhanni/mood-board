6e51e85232a6600180c777a58340126a10e3a1e7
Zack Hanni <zack.hannI@gmail.com>
2024-01-23 16:29:20 -0500

    create a route to  a user's getEntries

---
+++ app/api/auth/getEntries/route.ts
@@ -0,0 +1,27 @@
+import { connectToDatabase } from "@/helpers/server-helpers"
+import { NextRequest, NextResponse } from "next/server"
+import prisma from "@/prisma"
+
+
+export const GET = async (req: NextRequest) => {
+    try {
+        await connectToDatabase()
+        const userEmail = req.nextUrl.searchParams.get("email")
+        if (!userEmail) {
+            return NextResponse.json({ error: "Email parameter is required"}, { status: 400 })
+        }
+        const entries = await prisma.entry.findMany({
+            where: {
+                user: {
+                    email: userEmail
+                }
+            }
+        })
+        return NextResponse.json({ entries }, { status: 200 })
+    } catch (error) {
+        console.error(error); // Log the error
+        return NextResponse.json({ error: "Server Error" }, { status: 500 });
+    } finally {
+        await prisma.$disconnect()   
+    }
+}
