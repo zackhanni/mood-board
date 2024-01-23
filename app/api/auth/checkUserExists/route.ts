9f1ab3060a3aee8a7021d59e38eb4a81e8751e3c
Zack Hanni <zack.hannI@gmail.com>
2024-01-23 10:14:32 -0500

    creates route to check if a user exists (not working yet)

---
+++ app/api/auth/checkUserExists/route.ts
@@ -0,0 +1,27 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "@/app/lib/prisma";
+import { connectToDatabase } from "@/helpers/server-helpers";
+import { NextResponse } from "next/server";
+
+export const GET = async (req: NextApiRequest) => {
+   const { email } = req.body;
+
+   try {
+      await connectToDatabase()
+      const user = await prisma.user.findFirst({ where: { email } })
+      return NextResponse.json({ userExists: user !== null })
+
+   } 
+   catch (error) {
+      console.log(error);
+      return NextResponse.json({ message: "Server Error" }, { status: 500 });
+    } finally {
+      await prisma.$disconnect();
+    }
+
+
+}
+
+
+
+
