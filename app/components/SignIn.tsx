e2f67bf06e944fb69bd7e637c38c0f6a3ed4c248
Zack Hanni <zack.hannI@gmail.com>
2024-01-23 23:29:48 -0500

    combine google and credential signin into one signin component

--- app/components/SignIn.tsx
+++ app/components/SignIn.tsx
@@ -1,18 +1,36 @@
 "use client";
 
+import Image from "next/image";
+import googleLogo from "@/public/google.png";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+
+export function GoogleSignIn() {
+  const handleGoogleClick = () => {
+    signIn("google");
+  };
+
+  return (
+    <button
+      onClick={handleGoogleClick}
+      className="w-full flex items-center font-semibold justify-center h-14 px-6 mt-4 text-xl  transition-colors duration-300 bg-white hover:bg-black border-4 border-black text-black hover:text-white rounded-lg focus:shadow-outline hover:translate-y-1"
+    >
+      <Image src={googleLogo} alt="Google Logo" width={20} height={20} />
+      <span className="ml-4">Continue with Google</span>
+    </button>
+  );
+}
 
 interface CredentialsFormProps {
   csrfToken?: string;
 }
 
-export function CredentialsLogInForm(props: CredentialsFormProps) {
+export function CredentialsSignIn(props: CredentialsFormProps) {
   const router = useRouter();
   const [error, setError] = useState<string | null>(null);
 
-  const handleSubmit = async (e: React.FormEvent) => {
+  const handleCredentialsSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
     const data = new FormData(e.currentTarget as HTMLFormElement);
 
@@ -34,7 +52,7 @@ export function CredentialsLogInForm(props: CredentialsFormProps) {
   return (
     <form
       className="w-full mt-8 text-xl text-black font-semibold flex flex-col"
-      onSubmit={handleSubmit}
+      onSubmit={handleCredentialsSubmit}
     >
       {error && (
         <span className="p-4 mb-2 text-lg font-semibold text-white bg-red-500 rounded-md">
@@ -59,7 +77,7 @@ export function CredentialsLogInForm(props: CredentialsFormProps) {
 
       <button
         type="submit"
-        className="w-full h-14 px-6 mt-4 text-lg text-white hover:text-black transition-colors duration-150 bg-black rounded-lg focus:shadow-outline hover:bg-white hover:border-black hover:border-4"
+        className="w-full h-14 px-6 mt-4 text-lg hover:text-white text-black transition-colors duration-150 hover:bg-black rounded-lg focus:shadow-outline bg-white border-black border-4 hover:translate-y-1"
       >
         Log in
       </button>
