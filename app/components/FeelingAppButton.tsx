112ca4bcbf499f0789d6f755b8bab096baffa508
Zack Hanni <zack.hannI@gmail.com>
2024-01-28 01:53:16 -0500

    adjusted size and style of main button

--- app/components/FeelingAppButton.tsx
+++ app/components/FeelingAppButton.tsx
@@ -7,7 +7,7 @@ export default function FeelingAppButton(props: ButtonProps) {
   return (
     <button
       onClick={props.onClick}
-      className="px-4 py-2 flex w-full md:max-w-screen-sm font-bold rounded border-solid border-4 border-black hover:bg-yellow-100 items-center justify-center text-xl shadow-md hover:shadow-2xl active:bg-yellow-100"
+      className="px-3 py-2 font-bold rounded border-solid border-4 border-black hover:bg-yellow-100 shadow-md hover:shadow-2xl active:bg-yellow-100 hover:-translate-y-1 hover:underline"
     >
       {props.text}
     </button>
