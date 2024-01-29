1e4c34004290778bd2bc97c66aa4d94a7bff66bd
Zack Hanni <zack.hannI@gmail.com>
2024-01-29 00:00:13 -0500

    commended out api route. causing vercel to fail

--- app/api/auth/checkUserExists/route.ts
+++ app/api/auth/checkUserExists/route.ts
@@ -1,26 +1,25 @@
-import { NextApiRequest, NextApiResponse } from "next";
-import prisma from "@/app/lib/prisma";
-import { connectToDatabase } from "@/helpers/server-helpers";
-import { NextResponse } from "next/server";
+// import { NextApiRequest, NextApiResponse } from "next";
+// import prisma from "@/app/lib/prisma";
+// import { connectToDatabase } from "@/helpers/server-helpers";
+// import { NextResponse } from "next/server";
 
-export const GET = async (req: NextApiRequest) => {
-   const { email } = req.body;
+// export const GET = async (req: NextApiRequest) => {
+//    const { email } = req.body;
 
-   try {
-      await connectToDatabase()
-      const user = await prisma.user.findFirst({ where: { email } })
-      return NextResponse.json({ userExists: user !== null })
+//    try {
+//       await connectToDatabase()
+//       const user = await prisma.user.findFirst({ where: { email } })
+//       return NextResponse.json({ userExists: user !== null })
 
-   } 
-   catch (error) {
-      console.log(error);
-      return NextResponse.json({ message: "Server Error" }, { status: 500 });
-    } finally {
-      await prisma.$disconnect();
-    }
+//    } 
+//    catch (error) {
+//       console.log(error);
+//       return NextResponse.json({ message: "Server Error" }, { status: 500 });
+//     } finally {
+//       await prisma.$disconnect();
+//     }
 
-
-}
+// }
 
 
 
