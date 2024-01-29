7cb727e6803fe0154941ea251ad8b77f8323fe6b
Zack Hanni <zack.hannI@gmail.com>
2024-01-28 22:05:19 -0500

    made button change color depending on the question being asked

--- app/components/FeelingAppButton.tsx
+++ app/components/FeelingAppButton.tsx
@@ -1,13 +1,19 @@
 interface ButtonProps {
   onClick: (e: React.MouseEvent<HTMLButtonElement>) => void;
   text: string;
+  color?: string;
 }
 
 export default function FeelingAppButton(props: ButtonProps) {
+  console.log("Color prop:", props.color);
+  let hoverClass = props.color
+    ? `hover:bg-${props.color}`
+    : "hover:bg-yellow-100";
+
   return (
     <button
       onClick={props.onClick}
-      className="px-3 py-2 font-bold rounded border-solid border-4 border-black hover:bg-yellow-100 shadow-md hover:shadow-2xl active:bg-yellow-100 hover:-translate-y-1 hover:underline"
+      className={`px-3 py-2 font-bold rounded border-solid border-4 border-black ${hoverClass} shadow-md hover:shadow-2xl active:bg-yellow-100 hover:-translate-y-1 hover:underline`}
     >
       {props.text}
     </button>
