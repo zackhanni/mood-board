c6ccef4c91a09df17de370c2c96c3b5c1afefcb0
Zack Hanni <zack.hannI@gmail.com>
2024-01-28 23:19:36 -0500

    rename hoverClass to hoverColor

--- app/components/FeelingAppButton.tsx
+++ app/components/FeelingAppButton.tsx
@@ -5,15 +5,15 @@ interface ButtonProps {
 }
 
 export default function FeelingAppButton(props: ButtonProps) {
-  console.log("Color prop:", props.color);
-  let hoverClass = props.color
+  // console.log("Color prop:", props.color);
+  let hoverColor = props.color
     ? `hover:bg-${props.color}`
     : "hover:bg-yellow-100";
 
   return (
     <button
       onClick={props.onClick}
-      className={`px-3 py-2 font-bold rounded border-solid border-4 border-black ${hoverClass} shadow-md hover:shadow-2xl active:bg-yellow-100 hover:-translate-y-1 hover:underline`}
+      className={`px-3 py-2 font-bold rounded border-solid border-4 border-black ${hoverColor} shadow-md hover:shadow-2xl active:bg-yellow-100 hover:-translate-y-1 hover:underline`}
     >
       {props.text}
     </button>
