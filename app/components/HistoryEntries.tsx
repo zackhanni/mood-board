f7fa3eb22b75e6b0702f445bf4eef849e9e4e586
Zack Hanni <zack.hannI@gmail.com>
2024-02-02 10:57:36 -0500

    Update HistoryEntries.tsx

--- app/components/HistoryEntries.tsx
+++ app/components/HistoryEntries.tsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import { useSession } from "next-auth/react";
+import Calendar from "./Calendar";
 
 type Entry = {
   id: string;
@@ -32,44 +33,47 @@ export default function HistoryEntries() {
   }, [userEmail]);
 
   return (
-    <div>
-      {entries.length > 0 ? (
-        entries.toReversed().map((entry) => {
-          return (
-            <div
-              key={entry.id}
-              className="grid gap-3 border-4 border-black m-2 p-4 rounded-md grid-cols-1 md:grid-cols-2"
-            >
-              <div className="">
-                <p>
-                  You were feeling{" "}
-                  <span className="font-bold">{entry.feeling} </span> on
-                </p>
-                <p>
-                  {new Date(entry.createdAt).toLocaleDateString("en-US", {
-                    weekday: "short",
-                    year: "numeric",
-                    month: "long",
-                    day: "numeric",
-                    hour: "2-digit",
-                    minute: "2-digit",
-                  })}
-                </p>
-              </div>
+    <>
+      <Calendar data={entries} />
+      <div>
+        {entries.length > 0 ? (
+          entries.toReversed().map((entry) => {
+            return (
+              <div
+                key={entry.id}
+                className="grid gap-3 border-4 border-black m-2 p-4 rounded-md grid-cols-1 md:grid-cols-2"
+              >
+                <div className="">
+                  <p>
+                    You were feeling{" "}
+                    <span className="font-bold">{entry.feeling} </span> on
+                  </p>
+                  <p>
+                    {new Date(entry.createdAt).toLocaleDateString("en-US", {
+                      weekday: "short",
+                      year: "numeric",
+                      month: "long",
+                      day: "numeric",
+                      hour: "2-digit",
+                      minute: "2-digit",
+                    })}
+                  </p>
+                </div>
 
-              {entry.thoughts && (
-                <p>
-                  Your <span className="underline">thoughts</span>:
-                  <br />
-                  {entry.thoughts}
-                </p>
-              )}
-            </div>
-          );
-        })
-      ) : (
-        <p className="py-16">no entries yet or unable to fetch entries</p>
-      )}
-    </div>
+                {entry.thoughts && (
+                  <p>
+                    Your <span className="underline">thoughts</span>:
+                    <br />
+                    {entry.thoughts}
+                  </p>
+                )}
+              </div>
+            );
+          })
+        ) : (
+          <p className="py-16">no entries yet or unable to fetch entries</p>
+        )}
+      </div>
+    </>
   );
 }
