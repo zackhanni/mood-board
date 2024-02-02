ca3f5b1adc1f191345bc4ed7b0135ffdf9fc9ba6
Zack Hanni <zack.hannI@gmail.com>
2024-02-02 10:55:58 -0500

    buttons change color and receive a color prop

--- app/components/FeelingAppButton.tsx
+++ app/components/FeelingAppButton.tsx
@@ -5,15 +5,23 @@ interface ButtonProps {
 }
 
 export default function FeelingAppButton(props: ButtonProps) {
-  // console.log("Color prop:", props.color);
-  let hoverColor = props.color
-    ? `hover:bg-${props.color}`
-    : "hover:bg-yellow-100";
+  const colorMap = {
+    "yellow-100": "hover:bg-yellow-100",
+    "blue-100": "hover:bg-blue-100",
+    "red-100": "hover:bg-red-100",
+  };
+
+  let hoverColor;
+  if (props.color && colorMap[props.color]) {
+    hoverColor = colorMap[props.color];
+  } else {
+    hoverColor = "";
+  }
 
   return (
     <button
       onClick={props.onClick}
-      className={`px-3 py-2 font-bold rounded border-solid border-4 border-black ${hoverColor} shadow-md hover:shadow-2xl active:bg-yellow-100 hover:-translate-y-1 hover:underline`}
+      className={`px-3 py-2 font-bold rounded border-solid border-4 border-black ${hoverColor} shadow-md hover:shadow-2xl hover:-translate-y-1 hover:underline duration-300`}
     >
       {props.text}
     </button>
