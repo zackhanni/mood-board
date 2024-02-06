df3b6f71759e15d60f3edaa7e1b35b65b2d9d3dc
Zack Hanni <zack.hannI@gmail.com>
2024-02-05 22:14:07 -0500

    changed nav logout link to settings page

--- app/components/Nav.tsx
+++ app/components/Nav.tsx
@@ -1,33 +1,36 @@
 "use client";
 
 import Link from "next/link";
-import { signOut } from "next-auth/react";
 import { useSession } from "next-auth/react";
 
 export default function Nav() {
-  const handleSignOut = async () => {
-    await signOut({ callbackUrl: "/" });
-  };
   const { status } = useSession();
+
+  const navData = [
+    {
+      text: "Settings",
+      link: "/settings",
+    },
+    {
+      text: "Mental Check In",
+      link: "/mental-check-in",
+    },
+    {
+      text: "History",
+      link: "/history",
+    },
+  ];
 
   if (status == "authenticated") {
     return (
       <div className="h-16 w-full top-0 left-0 fixed flex bg-black sm:hover:bg-black text-white sm:hover:text-white justify-evenly sm:justify-center sm:space-x-16 items-center hover:shadow-2xl duration-300">
-        <Link href="/">
-          <p
-            className="font-bold hover:underline"
-            // figure out how to make signout work without onClick, it breaks because its a component in layout
-            // onClick={handleSignOut}
-          >
-            Log Out
-          </p>
-        </Link>
-        <Link href="/mental-check-in">
-          <p className="font-bold hover:underline">Mental Check In</p>
-        </Link>
-        <Link href="/history">
-          <p className="font-bold hover:underline">History</p>
-        </Link>
+        {navData.map((navLink) => {
+          return (
+            <Link href={navLink.link} key={navLink.text}>
+              <p className="font-bold hover:underline">{navLink.text}</p>
+            </Link>
+          );
+        })}
       </div>
     );
   }
