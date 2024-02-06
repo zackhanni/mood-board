dbf203beb56600a90d03f5cf818d831b9f304f88
Zack Hanni <zack.hannI@gmail.com>
2024-02-05 22:14:50 -0500

    added ability for user to change their username

--- app/components/UserData.tsx
+++ app/components/UserData.tsx
@@ -1,11 +1,31 @@
 import { useSession } from "next-auth/react";
 import Image from "next/image";
+import { useState } from "react";
+import axios from "axios";
 
 export default function UserData() {
   const { data: session } = useSession();
   const username = session?.user?.name || "Unable to fetch name";
   const email = session?.user?.email || "Unable to fetch email";
+  const [newName, setNewName] = useState("");
+  const [showNewNameInput, setShowNewNameInput] = useState(false);
   const profileImage = session?.user?.image;
+
+  const changeName = async (e: React.FormEvent) => {
+    // e.preventDefault();
+    try {
+      const updateUser = {
+        name: newName,
+        email: email,
+      };
+      // can i change axios out for prisma?
+      const response = await axios.post("/api/auth/changeName", updateUser);
+      setShowNewNameInput(false);
+      console.log("User updated successfully: ", response.data);
+    } catch (error) {
+      console.error("Error updating user: ", error);
+    }
+  };
 
   return (
     <div className="">
@@ -17,8 +37,35 @@ export default function UserData() {
           height={100}
         />
       ) : null}
-      <p className="">{username}</p>
-      <p className="">{email}</p>
+      <div className="flex flex-col items-center space-x-2 space-y-2">
+        <p className="">{username}</p>
+        <p
+          className="text-black/50 text-xs hover:underline"
+          onClick={() => setShowNewNameInput(true)}
+        >
+          (Change Name. Log in again to take effect)
+        </p>
+        {showNewNameInput && (
+          <form onSubmit={changeName}>
+            <input
+              type="text"
+              name="name"
+              placeholder="New Name"
+              required
+              value={newName}
+              onChange={(e) => setNewName(e.target.value)}
+              className="w-full px-4 py-3 mb-4 border border-gray-300 rounded-md"
+            />
+            <button
+              type="submit"
+              className="w-full py-2 px-4 bg-red-600 text-white hover:underline"
+            >
+              Save
+            </button>
+          </form>
+        )}
+        <p className="">{email}</p>
+      </div>
     </div>
   );
 }
