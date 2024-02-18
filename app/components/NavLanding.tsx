9ac63172fa92f198c3eb3150180ce25af2334355
Zack Hanni <zack.hannI@gmail.com>
2024-02-18 16:00:03 -0500

    created mobile friendly nav for landing page

--- app/components/NavLanding.tsx
+++ app/components/NavLanding.tsx
@@ -1,10 +1,12 @@
 "use client";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faRectangleXmark } from "@fortawesome/free-solid-svg-icons";
 import Link from "next/link";
-import { useSession } from "next-auth/react";
+import { useState } from "react";
 
 export default function LandingNav() {
-  const { status } = useSession();
+  const [showMobileNav, setShowMobileNav] = useState(false);
 
   const navData = [
     {
@@ -21,12 +23,60 @@ export default function LandingNav() {
     },
   ];
 
+  const toggleMobileNav = () => {
+    setShowMobileNav(!showMobileNav);
+  };
+
   return (
     <nav className="h-16 w-full top-0 fixed flex justify-between items-center px-8 bg-white/95 z-50">
-      <div className=" hover:bg-[#6C63FF] hover:text-white p-2 rounded-md duration-1000">
+      <div className="">
         <Link href="/">Mood Board</Link>
       </div>
-      <div className="flex justify-between space-x-6">
+
+      <div className="sm:hidden z-20">
+        {showMobileNav ? (
+          <FontAwesomeIcon
+            icon={faRectangleXmark}
+            size="lg"
+            onClick={() => toggleMobileNav()}
+          />
+        ) : (
+          <FontAwesomeIcon
+            icon={faBars}
+            size="lg"
+            style={{ color: "#000000" }}
+            onClick={() => toggleMobileNav()}
+          />
+        )}
+      </div>
+
+      <div
+        className={`${
+          showMobileNav ? "flex" : "hidden"
+        } fixed top-0 left-0 w-full h-full bg-white/50 backdrop-blur-sm items-center `}
+      >
+        <div className="flex flex-col mx-auto space-y-2 text-4xl">
+          {navData.map((navLink) => {
+            return (
+              <Link
+                href={navLink.link}
+                key={navLink.text}
+                onClick={() => toggleMobileNav()}
+              >
+                <p className="hover:underline">{navLink.text}</p>
+              </Link>
+            );
+          })}
+          <Link href="/connect" onClick={() => toggleMobileNav()}>
+            <p className="hover:underline">Log In</p>
+          </Link>
+          <Link href="/connect" onClick={() => toggleMobileNav()}>
+            <p className="hover:underline">Get Started</p>
+          </Link>
+        </div>
+      </div>
+
+      <div className="sm:flex hidden justify-between space-x-6">
         {navData.map((navLink) => {
           return (
             <Link href={navLink.link} key={navLink.text}>
@@ -35,7 +85,7 @@ export default function LandingNav() {
           );
         })}
       </div>
-      <div className="flex space-x-6 items-center">
+      <div className="sm:flex space-x-6 items-center hidden">
         <Link href="/connect">
           <p className="hover:underline">Log In</p>
         </Link>
