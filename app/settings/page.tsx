ac430f5bf1c192f114f81ce0bb23a911a0a6eb9a
Zack Hanni <zack.hannI@gmail.com>
2024-02-18 14:04:18 -0500

    replaces settings buttons. cleaned up formatting

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -2,6 +2,7 @@
 
 import { signOut } from "next-auth/react";
 import UserData from "../components/UserData";
+import Button from "../components/Button";
 
 export default function page() {
   const handleSignOut = () => {
@@ -11,23 +12,16 @@ export default function page() {
     <>
       <div className="flex flex-col justify-center items-center">
         <div className="max-w-sm py-16">
-          <p className="pb-4 underline">You are currently logged in as:</p>
+          <p className="pb-4 text-center">You are currently logged in as:</p>
           <UserData />
         </div>
         <div className="space-y-4 flex flex-col">
-          <button
-            className="bg-black text-white p-4 hover:underline"
+          <Button classes="bg-black" onClick={handleSignOut} text="Log Out" />
+          <Button
+            classes="bg-red-600"
             onClick={handleSignOut}
-          >
-            Log Out
-          </button>
-
-          <button
-            className="bg-red-600 text-white p-4 hover:underline"
-            onClick={handleSignOut}
-          >
-            Delete Account
-          </button>
+            text="Delete Account"
+          />
         </div>
       </div>
     </>
