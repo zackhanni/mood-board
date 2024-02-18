0084610c587df5ffb4c45fdace91dcfe5f2a859f
Zack Hanni <zack.hannI@gmail.com>
2024-02-18 15:59:23 -0500

    fixed icon sizes

--- app/components/HomeSection1.tsx
+++ app/components/HomeSection1.tsx
@@ -17,37 +17,37 @@ export default function HomeSection1() {
       title: "Comprehensive Emotion Education",
       content:
         "Comprehensive learning experiences with detailed information on a wide range of emotions. Delve into more complex and nuanced feelings.",
-      icon: <FontAwesomeIcon icon={faHeart} />,
+      icon: <FontAwesomeIcon size="xl" icon={faHeart} />,
     },
     {
       title: "Interactive Learning Modules",
       content:
         "Engaging and interactive learning modules that cater to different learning styles. Utilize quizzes, games, and real-life scenarios.",
-      icon: <FontAwesomeIcon icon={faPenToSquare} />,
+      icon: <FontAwesomeIcon size="xl" icon={faPenToSquare} />,
     },
     {
       title: "Personalized Emotion Tracking",
       content:
         "Customizable emotion tracking allows users to log and analyze their emotions over time. Identify patterns, triggers, and progress in managing emotions effectively.",
-      icon: <FontAwesomeIcon icon={faUser} />,
+      icon: <FontAwesomeIcon size="xl" icon={faUser} />,
     },
     {
       title: "Goal-Oriented Progress Tracking",
       content:
         "Set emotional well-being goals. Track progress, celebrate achievements, and see areas that may need improvement.",
-      icon: <FontAwesomeIcon icon={faCalendarDays} />,
+      icon: <FontAwesomeIcon size="xl" icon={faCalendarDays} />,
     },
     {
       title: "Expert Guidance and Resources",
       content:
         "Expert insights, articles, and resources on emotional well-being. Psychologists, therapists, and mental health professionals provide reliable information and tips on managing and understanding emotions.",
-      icon: <FontAwesomeIcon icon={faClipboard} />,
+      icon: <FontAwesomeIcon size="xl" icon={faClipboard} />,
     },
     {
       title: "Data Encryption",
       content:
         "All data transmitted between your device and our servers is encrypted using industry-standard protocols. This means that even if intercepted, the information remains unreadable to unauthorized parties.",
-      icon: <FontAwesomeIcon icon={faEyeSlash} />,
+      icon: <FontAwesomeIcon size="xl" icon={faEyeSlash} />,
     },
   ];
 
@@ -66,7 +66,7 @@ export default function HomeSection1() {
                 key={item.title}
                 className="bg-white/50 p-4 rounded-xl text-xs backdrop-blur-md "
               >
-                <div className="w-6 h-auto mx-auto">{item.icon}</div>
+                <div className="text-center">{item.icon}</div>
 
                 <h3 className="py-1 text-base leading-5 pt-2 font-medium">
                   {item.title}
