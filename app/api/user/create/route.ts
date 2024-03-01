7cd85dfb3e8132592e3120c27db183c6f411967b
Zack Hanni <zack.hannI@gmail.com>
2024-03-01 18:58:31 -0500

    added potential code for error handling and validation

--- app/api/user/create/route.ts
+++ app/api/user/create/route.ts
@@ -6,8 +6,39 @@ import bcrypt from "bcrypt";
 // create a new user
 // POST api/user/update
 export const POST = async (req: Request) => {
+  const { name, email, password } = await req.json();
+
   try {
-    const { name, email, password } = await req.json();
+    // const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    // if (!emailRegex.test(email)) {
+    //   return NextResponse.json(
+    //     { message: "Invalid email format" },
+    //     { status: 422 }
+    //   );
+    // }
+
+    // const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$/;
+    // if (!passwordRegex.test(password)) {
+    // return NextResponse.json({ message: "Password must be at least 8 characters long and include at least one uppercase letter, one lowercase letter, and one number" }, { status: 422 })
+    //}
+
+    // if (!name) {
+    //   return NextResponse.json(
+    //     { message: "Name is required" },
+    //     { status: 422 }
+    //   );
+    // } else if (!email) {
+    //   return NextResponse.json(
+    //     { message: "Email is required" },
+    //     { status: 422 }
+    //   );
+    // } else if (!password) {
+    //   return NextResponse.json(
+    //     { message: "Password is required" },
+    //     { status: 422 }
+    //   );
+    // }
+
     if (!name || !email || !password) {
       console.log(
         `Validation failed: name=${name}, email=${email}, password=${password}`
@@ -15,12 +46,12 @@ export const POST = async (req: Request) => {
       return NextResponse.json({ message: "Invalid Data" }, { status: 422 });
     }
 
-    const existingUser = await prisma.user.findUnique({
+    const userExists = await prisma.user.findUnique({
       where: {
         email: email,
       },
     });
-    if (existingUser) {
+    if (userExists) {
       console.error("Email already in use");
       return NextResponse.json(
         { message: "Email already in use (in /create/route)" },
@@ -28,10 +59,9 @@ export const POST = async (req: Request) => {
       );
     }
 
-    // encrypt the password before going to db
-    const hashedPassword = await bcrypt.hash(password, 10);
     await connectToDatabase();
     console.log(`Connected to database to create a user`);
+    const hashedPassword = await bcrypt.hash(password, 10);
     //create the user
     const user = await prisma.user.create({
       data: {
