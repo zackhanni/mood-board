0276f07a0e8ab320297657479a64ddc1a6ba9e68
Zack Hanni <zack.hannI@gmail.com>
2024-03-06 22:10:25 -0500

    add callbacks to save add id to session.user with jwt

--- app/lib/auth.ts
+++ app/lib/auth.ts
@@ -26,49 +26,57 @@ export const authConfig: NextAuthOptions = {
       async authorize(credentials) {
         if (!credentials || !credentials.email || !credentials.password)
           return null;
-         
-          try {
-            await connectToDatabase()
-            const user = await prisma.user.findFirst(
-              {where: { email: credentials.email }
-            });
 
-            if (!user?.hashedPassword) {
-              return null
-            }
+        try {
+          await connectToDatabase();
+          const user = await prisma.user.findFirst({
+            where: { email: credentials.email },
+          });
 
-            const isPasswordCorrect = await bcrypt.compare(
-              credentials.password, 
-              user.hashedPassword
-            )
-            if (isPasswordCorrect) {
-              return user
-            }
-
-            return null
-          } catch (error) {
-            console.log(error)
-            return null
-          } finally {
-            await prisma.$disconnect()
+          if (!user?.hashedPassword) {
+            return null;
           }
+
+          const passwordIsCorrect = await bcrypt.compare(
+            credentials.password,
+            user.hashedPassword
+          );
+
+          if (passwordIsCorrect) {
+            console.log(`the authorize user object is:`);
+            console.log(JSON.stringify(user, null, 2));
+            return user;
+          }
+
+          return null;
+        } catch (error) {
+          console.log(error);
+          return null;
+        }
+        // finally {
+        //   await prisma.$disconnect();
+        // }
       },
     }),
   ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    session({ session, token }) {
+      if (token && session.user) {
+        session.user.id = token.id;
+        console.log("User ID stored in session.user.id");
+        console.log(session.user.id);
+      } else {
+        console.error("User is undefined in session callback");
+      }
+      return session;
+    },
+  },
+
   secret: process.env.NEXTAUTH_URL,
-  }
-
-
-// double check these 2 functions and where they are being called.
-// export async function loginIsRequiredServer() {
-//   const session = await getServerSession(authConfig);
-//   if (!session) return redirect("/");
-// }
-
-// export function loginIsRequiredClient() {
-//   if (typeof window !== "undefined") {
-//     const session = useSession();
-//     const router = useRouter();
-//     if (!session) router.push("/");
-//   }
-// }
+};
